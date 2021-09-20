748b0f593828a0d0dcaec51ced625cb312a3549f
Alberto Fernandes <albertohf91@gmail.com>
2021-09-20 18:18:22 -0300

    scroll

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
 window.addEventListener('scroll', () => {
-  if (window.screenY >= navHeight) {
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
